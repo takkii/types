8401c2e9d3fd2ec2cc22d9a1934dfff27fed6287
Ｔ・Ｋ <karuma.reason@gmail.com>
2025-12-25 15:23:06 +0900

    Update.

---
+++ mix.exs
@@ -0,0 +1,28 @@
+defmodule Types.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :types,
+      version: "0.0.1",
+      elixir: "~> 1.19",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:httpoison, "~> 2.0"},
+      {:json, "~> 1.4"}
+    ]
+  end
+end
